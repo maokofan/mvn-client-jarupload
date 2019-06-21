0661ba20033a6ad27eed0bf7caafab3137deb323
fanpei <fanpei1413@163.com>
2019-06-21 14:30:02 +0800

    优化注释显示

--- src/main/java/maoko/jarupload/UploadJarFiles.java
+++ src/main/java/maoko/jarupload/UploadJarFiles.java
@@ -2,7 +2,6 @@ package maoko.jarupload;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -23,12 +22,12 @@ import maoko.jarupload.sys.os.OSPlatformUtil;
 public class UploadJarFiles implements Runnable {
 
 	private static Logger logger = null;
-	private static Pattern DATE_PATTERN = null;
+	// private static Pattern DATE_PATTERN = null;
 	public static File TMP_DIR;// 文件临时根目录
 
 	public static void init() throws Exception {
 		logger = LoggerFactory.getLogger(UploadJarFiles.class);
-		DATE_PATTERN = Pattern.compile("-[\\d]{8}\\.[\\d]{6}-");
+		// DATE_PATTERN = Pattern.compile("-[\\d]{8}\\.[\\d]{6}-");
 		String tmpDir = null;
 		EOsType ostype = OSPlatformUtil.getOSType();
 		if (EOsType.Linux == ostype) {
@@ -60,12 +59,9 @@ public class UploadJarFiles implements Runnable {
 			File source = null;
 			File javadoc = null;
 			File[] files = jarDir.listFiles();
-			// 忽略日期快照版本，如 xxx-mySql-2.2.6-20170714.095105-1.jar
 			for (File file : files) {
 				String name = file.getName();
-				if (DATE_PATTERN.matcher(name).find()) {
-					// skip
-				} else if (name.endsWith(".pom")) {
+				if (name.endsWith(".pom")) {
 					pom = file;
 				} else if (name.endsWith("-javadoc.jar")) {
 					javadoc = file;
@@ -78,7 +74,7 @@ public class UploadJarFiles implements Runnable {
 			if (pom != null || jar != null)// 限制条件，防止93行file为空
 				deploy(pom, jar, source, javadoc);
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error("意外错误", e);
 		}
 	}
 
@@ -91,19 +87,22 @@ public class UploadJarFiles implements Runnable {
 	public void deploy(final File pom, final File jar, final File source, final File javadoc) throws IOException {
 		String cmdStr = MvnCmd.getFullCmdStr(pom, jar, source, javadoc);
 		File file = jar == null ? pom : jar;
-		int result = upload(file.getAbsolutePath(), file.getParentFile(), cmdStr);
+		int result = upload(file.getAbsolutePath(), file.getParentFile(), cmdStr, false);
 		if (result != 0) {// 重试
-			logger.info("移动目录后重试上传:" + file.getAbsolutePath());
-			logger.info("starting copy files to tmp dir");
 			StringBuilder newdirsb = new StringBuilder(TMP_DIR.getAbsolutePath()).append(File.separator);
 			String newJarPath = newdirsb.append(file.getName()).toString();
 			String newdir = newJarPath.replace(".jar", "");
 			File destTmp = new File(newdir);
 			try {
+				logger.info(
+						"\r\n\r\n========================================================\r\n移动目录重试上传 starting copy files [{}] to tmp dir [{}]",
+						file.getParentFile(), destTmp);
 				// 复制至临时目录
 				FileUtils.copyDirectory(file.getParentFile(), destTmp);
-				logger.info("sucessful copy files to tmp dir");
-				upload(newJarPath, destTmp, cmdStr);
+				logger.info(
+						"\r\n\r\n========================================================\r\n移动目录重试上传 sucessful copy files [{}] to tmp dir [{}]",
+						file.getParentFile(), destTmp);
+				upload(newJarPath, destTmp, cmdStr, true);
 			} finally {
 				// 上传完后删除
 				FileUtils.deleteDirectory(destTmp);
@@ -116,25 +115,31 @@ public class UploadJarFiles implements Runnable {
 	 * @param cmd
 	 * @return 0:成功
 	 */
-	private int upload(final String filePath, final File workDir, String cmdStr) {
+	private int upload(final String filePath, final File workDir, String cmdStr, boolean reDeploy) {
 		int result = 1;
 		Process proc = null;
 		try {
-			logger.info("\r\n========================================================\r\n执行命令:" //
-					+ cmdStr //
-					+ "\r\n========================================================");
+			logger.info(
+					"\r\n\r\n========================================================\r\n准备上传:{}\r\n-----------------------------------------------\r\n执行命令:{}\r\n========================================================\r\n\r\n",
+					filePath, cmdStr);
 			proc = Runtime.getRuntime().exec(cmdStr, null, workDir);
 			// 线程读取输出流
 			TheadPoolExc.excutePrint(new StandardStreamPrint(proc.getInputStream()));
 			TheadPoolExc.excutePrint(new StandardStreamPrint(proc.getErrorStream()));
 			result = proc.waitFor();
 			if (result != 0) {
-				logger.error("上传失败:{}", filePath);
-			} else
-				logger.info("上传成功:{}", filePath);
+				logger.error("\r\n\r\n========================================================\r\n上传失败:{}", filePath);
+			} else {
+				if (reDeploy) {
+					logger.info("\r\n\r\n========================================================\r\n重试上传成功:{}",
+							filePath);
+				} else
+					logger.info("\r\n\r\n========================================================\r\n上传成功:{}",
+							filePath);
+			}
 			System.out.println(System.lineSeparator());
 		} catch (Exception e) {
-			logger.error("上传失败：{}", filePath, e);
+			logger.error("\r\n\r\n========================================================\r\n上传失败：{}", filePath, e);
 			e.printStackTrace();
 		} finally {
 			if (proc != null)
--- src/main/java/maoko/jarupload/conf/MvnCmd.java
+++ src/main/java/maoko/jarupload/conf/MvnCmd.java
@@ -14,6 +14,7 @@ import maoko.jarupload.AppUploadJar;
  */
 public class MvnCmd {
 	private static final String DOS_CMD = "cmd /c mvn" //
+			+ "  -X"//
 			+ " -s {0}" //
 			+ " deploy:deploy-file"//
 			+ " -Durl={1}"//
